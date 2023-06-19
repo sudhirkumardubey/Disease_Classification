95fd0b72528370f65f941db622f581d311246ec4
sudhirkumardubey <dubey.sudhirpp@gmail.com>
2023-06-19 13:33:51 +0200

    Evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from CNN_Classifier.pipeline.stage_03_training import ModelTrainingPipeline
+from CNN_Classifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -31,10 +33,22 @@ except Exception as e:
 STAGE_NAME = "Callbacks and Training"
 try: 
    logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   logger.info(f">>>>>> stage 3 {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage 3 {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Evaluation"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage 4 {STAGE_NAME} started <<<<<<")
+   model_trainer = EvaluationPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage 4 {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
--- src/CNN_Classifier/config/configuration.py
+++ src/CNN_Classifier/config/configuration.py
@@ -1,12 +1,15 @@
 # to read yaml files
 import os
 from CNN_Classifier.constants import *
+from pathlib import Path
 from CNN_Classifier.utils.common import read_yaml, create_directories
 
 from CNN_Classifier.entity.config_entity import (DataIngestionConfig, 
                                                  PrepareBaseModelConfig, 
                                                  PrepareCallbacksConfig,
-                                                 TrainingConfig)
+                                                 TrainingConfig,
+                                                 EvaluationConfig
+                                                 )
 
 
 """
@@ -104,5 +107,16 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        
+        return eval_config
     
 
