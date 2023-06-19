5bfa7a2b233550063bb2736a55a7aa188560d149
sudhirkumardubey <dubey.sudhirpp@gmail.com>
2023-06-19 12:54:20 +0200

    Callbacks and Training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNN_Classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -22,6 +23,18 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage 2 {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Callbacks and Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
---
+++ src/CNN_Classifier/pipeline/stage_03_training.py
@@ -0,0 +1,37 @@
+from CNN_Classifier.config.configuration import ConfigurationManager
+from CNN_Classifier.components.prepare_callbacks import PrepareCallback
+from CNN_Classifier.components.training import Training
+from CNN_Classifier import logger
+
+
+STAGE_NAME = "Callbacks and Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_callbacks_config = config.get_prepare_callback_config()
+        prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(
+            callback_list=callback_list
+        )
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage 3 & 4: {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage 3 & 4: {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
